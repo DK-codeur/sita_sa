541f4db476a460c7696b70cb2319be9d08428099
DK-codeur <47182035+DK-codeur@users.noreply.github.com>
2020-09-09 12:28:16 +0000

    some logo of le groupe added

--- views/le-groupe_view.php
+++ views/le-groupe_view.php
@@ -45,27 +45,27 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/coplaci.png" alt="image"></div>
                                         <div class="service2_content">
-                                            <h3>CEDAICI</h3>
+                                            <h3>COPLACI</h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/soto.png" alt="image"></div>
                                         <div class="service2_content">
-                                            <h3>CEDAICI</h3>
+                                            <h3>SOTO</h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/iris.png" alt="image"></div>
                                         <div class="service2_content">
-                                            <h3>CEDAICI</h3>
+                                            <h3>IRIS</h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
                                         </div>
                                     </div>
@@ -74,9 +74,9 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/first.png" alt="image"></div>
                                         <div class="service2_content">
-                                            <h3>CEDAICI</h3>
+                                            <h3>FIRST</h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
                                         </div>
                                     </div>
