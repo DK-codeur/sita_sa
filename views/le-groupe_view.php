7eff7fa0294432e81a9dbcaeab95dd61193d65f9
ravivi <47162716+ravivi@users.noreply.github.com>
2020-09-09 12:56:03 +0200

    image octave

--- views/le-groupe_view.php
+++ views/le-groupe_view.php
@@ -34,7 +34,7 @@
 						<div class="clv_heading">
 							<h3>Groupe SITA S.A.</h3>
 							<div class="clv_underline"><img src="assets/images/agri_underline.png" alt="image" /></div>
-							<p>Consectetur adipisicing elit sed do eiusmod tempor incididunt ut labore et dole magna aliqua. Ut enim ad minim veniam quis nostrud exercitation.</p>
+							<p> La Societe Ivoirienne de Traitement d'Anacarde (SITA SA) regroupe en son sein 07 sociétés.</p>
 						</div>
 					</div>
                 </div>
@@ -45,7 +45,7 @@
                             <div class="row">
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/coplaci.png" alt="image"></div>
                                         <div class="service2_content">
                                             <h3>CEDAICI</h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
@@ -54,9 +54,9 @@
                                 </div>
                                 <div class="col-md-4">
                                     <div class="service2_block">
-                                        <div class="service2_image"><img src="assets/images/garden_service6.png" alt="image"></div>
+                                        <div class="service2_image"><img src="assets/images/co.png" alt="image"></div>
                                         <div class="service2_content">
-                                            <h3>CEDAICI</h3>
+                                            <h3>COPLACI/h3>
                                             <p>Dolor sit amet consectetur adipisicing elit sed do eiusmod tempor.</p>
                                         </div>
                                     </div>
