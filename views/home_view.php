2c9816df8d9a2de09e9c16cf6c2fc9cf43a74307
ravivi <47162716+ravivi@users.noreply.github.com>
2020-09-10 11:24:52 +0200

    include testimonials

--- views/home_view.php
+++ views/home_view.php
@@ -477,8 +477,8 @@ Aussi, mobiliser des ressources pour investir dans le developpement de toute la 
 			</div>
 		</div>
 
-		<!-- Nos clients -->
-		<div class="vision-title mb-4">
+		<!-- Nos Partenaire-->
+		<!-- <div class="vision-title mb-4">
 			<h1 class="text-center">Nos Partenaires</h1>
 		</div>
 
@@ -486,9 +486,11 @@ Aussi, mobiliser des ressources pour investir dans le developpement de toute la 
 			<p>
 				Au cours de son parcours, la SITA SA, par la qualité de son travail s'est entourée de plusieurs partenaires de renommée divers dont: 
 			</p>
-		</div>
+		</div> -->
 
-		<?php include_once 'views/includes/partners.php' ?>
+		<?php 
+		#include_once 'views/includes/partners.php' 
+		?> 
 
 		<!-- counter -->
 		<div class="counter">
@@ -560,89 +562,10 @@ Aussi, mobiliser des ressources pour investir dans le developpement de toute la 
 
 
 		<!-- talk about -->
-		<div class="agri_testimonial_wrapper clv_section">
-			<div class="container">
-				<div class="row">
-					<div class="col-md-4 col-lg-4">
-						<div class="agri_testimonial_content">
-							<h3>Que Disent Les Gens À Notre Propos<span><img src="assets/images/agri_underline3.png" alt="image"></span></h3>
-							<p>Consectetur adipisicing elit sed do eiusmod tempor  dunt ut labore et dole magna aliqua. </p>
-						</div>
-					</div>
-					<div class="col-md-8 col-lg-8">
-						<div class="agri_testimonial_slider">
-							<!-- Swiper -->
-							<div class="swiper-container swiper-container-initialized swiper-container-horizontal">
-								<div class="swiper-wrapper" style="transform: translate3d(-2460px, 0px, 0px); transition-duration: 0ms;"><div class="swiper-slide swiper-slide-duplicate swiper-slide-next swiper-slide-duplicate-prev" data-swiper-slide-index="1" style="width: 770px; margin-right: 50px;">
-										<div class="agri_testimonial_slide">
-											<div class="agri_testimonial_image">
-												<img src="https://via.placeholder.com/188x238" alt="image">
-											</div>
-											<div class="agri_testimonial_message">
-												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempeior incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quisesa fei nostrud exercitation ullamco laboris aliquip.</p>
-												<h4>Merica Ven <span>Garderner</span></h4>
-											</div>
-											<span class="agri_quate"><img src="assets/images/Quote-Icon.png" alt="image"></span>
-										</div>
-									</div>
-									<div class="swiper-slide swiper-slide-duplicate-active" data-swiper-slide-index="0" style="width: 770px; margin-right: 50px;">
-										<div class="agri_testimonial_slide">
-											<div class="agri_testimonial_image">
-												<img src="https://via.placeholder.com/188x238" alt="image">
-											</div>
-											<div class="agri_testimonial_message">
-												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempeior incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quisesa fei nostrud exercitation ullamco laboris aliquip.</p>
-												<h4>Merica Ven <span>Garderner</span></h4>
-											</div>
-											<span class="agri_quate"><img src="assets/images/Quote-Icon.png" alt="image"></span>
-										</div>
-									</div>
-									<div class="swiper-slide swiper-slide-prev swiper-slide-duplicate-next" data-swiper-slide-index="1" style="width: 770px; margin-right: 50px;">
-										<div class="agri_testimonial_slide">
-											<div class="agri_testimonial_image">
-												<img src="https://via.placeholder.com/188x238" alt="image">
-											</div>
-											<div class="agri_testimonial_message">
-												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempeior incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quisesa fei nostrud exercitation ullamco laboris aliquip.</p>
-												<h4>Merica Ven <span>Garderner</span></h4>
-											</div>
-											<span class="agri_quate"><img src="assets/images/Quote-Icon.png" alt="image"></span>
-										</div>
-									</div>
-								<div class="swiper-slide swiper-slide-duplicate swiper-slide-active" data-swiper-slide-index="0" style="width: 770px; margin-right: 50px;">
-										<div class="agri_testimonial_slide">
-											<div class="agri_testimonial_image">
-												<img src="https://via.placeholder.com/188x238" alt="image">
-											</div>
-											<div class="agri_testimonial_message">
-												<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempeior incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam quisesa fei nostrud exercitation ullamco laboris aliquip.</p>
-												<h4>Merica Ven <span>Garderner</span></h4>
-											</div>
-											<span class="agri_quate"><img src="assets/images/Quote-Icon.png" alt="image"></span>
-										</div>
-									</div></div>
-							<span class="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
-							<!-- Add Arrows -->
-							<div class="agri_arrow agri_testimonial_left" tabindex="0" role="button" aria-label="Previous slide">
-								<span>
-									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="10px" height="15px">
-									<path fill-rule="evenodd" fill="rgb(222, 222, 222)" d="M0.324,8.222 L7.117,14.685 C7.549,15.097 8.249,15.097 8.681,14.685 C9.113,14.273 9.113,13.608 8.681,13.197 L2.670,7.478 L8.681,1.760 C9.113,1.348 9.113,0.682 8.681,0.270 C8.249,-0.139 7.548,-0.139 7.116,0.270 L0.323,6.735 C0.107,6.940 -0.000,7.209 -0.000,7.478 C-0.000,7.747 0.108,8.017 0.324,8.222 Z"></path>
-									</svg>
-								</span>
-							</div>
-							<div class="agri_arrow agri_testimonial_right" tabindex="0" role="button" aria-label="Next slide">
-								<span>
-									<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="19px" height="25px">
-									<path fill-rule="evenodd" fill="rgb(222, 222, 222)" d="M13.676,13.222 L6.883,19.685 C6.451,20.097 5.751,20.097 5.319,19.685 C4.887,19.273 4.887,18.608 5.319,18.197 L11.329,12.478 L5.319,6.760 C4.887,6.348 4.887,5.682 5.319,5.270 C5.751,4.861 6.451,4.861 6.884,5.270 L13.676,11.735 C13.892,11.940 14.000,12.209 14.000,12.478 C14.000,12.747 13.892,13.017 13.676,13.222 Z"></path>
-									</svg>
-								</span>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-
+	
+		<?php 
+		#include_once 'views/includes/testimonials.php' 
+		?> 
 
 
 		
